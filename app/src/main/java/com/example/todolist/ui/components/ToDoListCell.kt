e322427a7d5b1acdc16d4a1ef1b84def28473211
NOGUEN <nhg1113@naver.com>
2024-06-05 20:14:42 +0900

    feat: 컨테이너 디자인 추가

--- app/src/main/java/com/example/todolist/ui/components/ToDoListCell.kt
+++ app/src/main/java/com/example/todolist/ui/components/ToDoListCell.kt
@@ -1,18 +1,38 @@
 package com.example.todolist.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ToDoListCell(
-    modifier: Modifier
+    cornerRadius: Dp = 12.dp,
+    backgroundColor: Color = Color.White,
+    titleText: String,
+    status: String,
 ) {
-    Column(
-        modifier = modifier,
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(4.dp, shape = RoundedCornerShape(cornerRadius))
+            .background(backgroundColor)
     ) {
-        Text(text = "Test ToDoListCell")
-        Text(text = "status")
+        Column(
+            Modifier.padding(horizontal = 10.dp, vertical = 10.dp)
+        ) {
+            Text(text = titleText, fontSize = 16.sp)
+            Text(text = status, fontSize = 14.sp)
+        }
     }
 }
--- app/src/main/java/com/example/todolist/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/todolist/ui/screen/MainScreen.kt
@@ -2,7 +2,9 @@
 
 package com.example.todolist.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -10,6 +12,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.components.ToDoListCell
 import com.example.todolist.ui.theme.ToDoListTheme
 
@@ -24,9 +29,38 @@ fun MainScreen() {
             },
             content = {
                 Column(
-                    modifier = Modifier.padding(it)
+                    modifier = Modifier
+                        .padding(it)
+                        .padding(horizontal = 10.dp,)
                 ) {
-                    ToDoListCell(modifier = Modifier.padding(it))
+                    Box(modifier = Modifier
+                        .padding(top = 10.dp)) {
+                        ToDoListCell(
+                            titleText = "Title",
+                            status = "OnGoing",
+                        )
+                    }
+                    Box(modifier = Modifier
+                        .padding(top = 10.dp)) {
+                        ToDoListCell(
+                            titleText = "Title",
+                            status = "OnGoing",
+                        )
+                    }
+                    Box(modifier = Modifier
+                        .padding(top = 10.dp)) {
+                        ToDoListCell(
+                            titleText = "Title",
+                            status = "OnGoing",
+                        )
+                    }
+                    Box(modifier = Modifier
+                        .padding(top = 10.dp)) {
+                        ToDoListCell(
+                            titleText = "Title",
+                            status = "OnGoing",
+                        )
+                    }
                 }
             }
         )
